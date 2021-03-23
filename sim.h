24452d26b8b44a3e86b77f85fa0a38ab9866d293
xiaotian <xiaotian.li5@student.unimelb.edu.au>
2021-03-23 12:02:09 +0800

    reformat codes

--- sim.h
+++ sim.h
@@ -4,6 +4,8 @@
 
 #ifndef COMP30023_2021_PROJECT_1_SIM_H
 #define COMP30023_2021_PROJECT_1_SIM_H
+#include "process.h"
+#include "myHashtable.h"
 
 void SimRun(input_node_ptr input_list_head, int numMainProcess, int numCPU, int useOwnScheduler);
 input_node_ptr ReadFile(const char *fileName, FILE *fp);
@@ -13,4 +15,35 @@ int CountLines(FILE *fp, int lineCounter);
 void
 GetArgs(int argc, char **argv, char **charPart, int argChar, int *useOwnScheduler, int *numProcessors, char **fileName);
 
+void HandleOneCPU(input_node_ptr input_list_head, int numMainProcess, input_node_ptr input_process_ptr,
+                  unsigned int *globalTimer, unsigned int *totalDeltaTime, int *numRunnedProcesses,
+                  double *maxTimeOverhead, double *totalTimeOverhead);
+
+void HandleMultiCPU(input_node_ptr input_list_head, int numMainProcess, int numCPU, HashTable *parallelProcTable,
+                    input_node_ptr input_process_ptr, unsigned int *globalTimer, unsigned int *totalDeltaTime,
+                    int *numRunnedProcesses, double *maxTimeOverhead, double *totalTimeOverhead);
+
+
+process_t *
+HandleArrivingProcOnOneCpu(input_node_ptr input_list_head, input_node_ptr input_process_ptr, unsigned int *globalTimer,
+                           unsigned int *totalDeltaTime, int *numRunnedProcesses, double *maxTimeOverhead,
+                           double *totalTimeOverhead, PQ_t pq, process_t *minRemainTimeProcess_ptr);
+
+void HandleRemainProcOnOneCPU(unsigned int *globalTimer, unsigned int *totalDeltaTime, int *numRunnedProcesses,
+                              double *maxTimeOverhead, double *totalTimeOverhead, PQ_t pq,
+                              process_t *minRemainTimeProcess_ptr);
+
+void InitMemCpuForMultiProc(int numMainProcess, int numCPU, HashTable *parallelProcTable, PQ_t **cpuPQList,
+                            process_t ***cpuMinProcess_ptr_list);
+
+void HandleArrivingProcOnMultiCpu(input_node_ptr input_list_head, int numCPU, HashTable *parallelProcTable,
+                                  input_node_ptr input_process_ptr, unsigned int *globalTimer,
+                                  unsigned int *totalDeltaTime, int *numRunnedProcesses, double *maxTimeOverhead,
+                                  double *totalTimeOverhead, int procCounter, PQ_t *cpuPQList,
+                                  process_t **cpuMinProcess_ptr_list);
+
+void HandleRemainProcOnMultiCPU(int numCPU, HashTable *parallelProcTable, unsigned int *globalTimer,
+                                unsigned int *totalDeltaTime, int *numRunnedProcesses, double *maxTimeOverhead,
+                                double *totalTimeOverhead, int procCounter, PQ_t *cpuPQList,
+                                process_t **cpuMinProcess_ptr_list);
 #endif //COMP30023_2021_PROJECT_1_SIM_H
