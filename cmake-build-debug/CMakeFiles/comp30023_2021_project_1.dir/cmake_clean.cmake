48f53302934a28c801502fa9c0183ddd5ffc42d6
Blackmesa-Canteen <shaotienlee@foxmail.com>
2021-07-04 23:38:38 +0800

    add files

---
+++ cmake-build-debug/CMakeFiles/comp30023_2021_project_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/comp30023_2021_project_1.dir/linkedlist.c.o"
+  "CMakeFiles/comp30023_2021_project_1.dir/main.c.o"
+  "CMakeFiles/comp30023_2021_project_1.dir/myHashtable.c.o"
+  "CMakeFiles/comp30023_2021_project_1.dir/process.c.o"
+  "CMakeFiles/comp30023_2021_project_1.dir/readFile.c.o"
+  "CMakeFiles/comp30023_2021_project_1.dir/sim.c.o"
+  "comp30023_2021_project_1"
+  "comp30023_2021_project_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/comp30023_2021_project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
